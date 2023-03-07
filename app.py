63779a2b65e9288c6cce1864201e084c72cb8b5b
Robert Carr <86175637+rhc-iv@users.noreply.github.com>
2023-03-07 16:02:00 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+# Import statements:
+import requests
+import streamlit as st
+
+# Save the API endpoint from CocktailDB to a variable:
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+# Make a GET request to the API to retrieve the ingredients list:
+response = requests.get(url)
+data = response.json()
+ingredients = [d['strIngredient1'] for d in data['drinks']]
+
+# Sort the ingredients alphabetically
+ingredients.sort()
+
+# Create a Streamlit selectbox with the sorted ingredients list:
+selected_ingredient = st.sidebar.selectbox(
+    'Select an ingredient:',
+    ingredients
+)
+
+# Make a GET request to the API to retrieve the drinks list for the selected
+# ingredient:
+selected_drinks_url = f'https://www.thecocktaildb.com/api/json/v1/1/filter' \
+                      f'.php?i={selected_ingredient}'
+selected_drinks_response = requests.get(selected_drinks_url)
+selected_drinks_data = selected_drinks_response.json()
+selected_drinks = [d['strDrink'] for d in selected_drinks_data['drinks']]
+
+# Create a Streamlit selectbox with the drinks list for the selected ingredient:
+selected_drink = st.sidebar.selectbox(
+    'Select a drink:',
+    selected_drinks
+)
+
+# Create a Submit button:
+if st.sidebar.button('Submit'):
+    # Make a GET request to the API to retrieve the details of the selected
+    # drink:
+    selected_drink_url = f'https://www.thecocktaildb.com/api/j' \
+                         f'son/v1/1/search.php?s={selected_drink}'
+    selected_drink_response = requests.get(selected_drink_url)
+    selected_drink_data = selected_drink_response.json()
+    selected_drink_details = selected_drink_data['drinks'][0]
+
+    # Display the selected drink details in the main Streamlit window:
+    st.header('Cocktail Details')
+    st.subheader(selected_drink_details['strDrink'].title())
+    st.image(selected_drink_details['strDrinkThumb'], width=200)
+    st.markdown('**Category:** ' + selected_drink_details['strCategory'])
+    st.markdown('**Glass:** ' + selected_drink_details['strGlass'])
+    st.markdown('**Ingredients:**')
+    for i in range(1, 16):
+        ingredient_name = selected_drink_details[f'strIngredient{i}']
+        if ingredient_name:
+            ingredient_measurement = selected_drink_details[f'strMeasure{i}']
+            st.write(f'- **{ingredient_name}** {ingredient_measurement}')
+    st.markdown(
+        '**Instructions:** ' + selected_drink_details['strInstructions'])
